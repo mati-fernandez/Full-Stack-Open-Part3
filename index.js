15e082cbaf325cae0bc15098a95b17c73fdd68ae
Matias Fernandez <matifer.dev@gmail.com>
2024-03-04 15:59:15 -0300

    Listo para despliegue en internet

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const morgan = require('morgan');
 const app = express();
+const cors = require('cors');
 
 let persons = [
   {
@@ -27,6 +28,7 @@ let persons = [
 
 app.use(express.json());
 app.use(morgan('tiny'));
+app.use(cors());
 
 morgan.token('data', (req) => {
   return JSON.stringify(req.body);
